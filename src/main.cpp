ac508be7b218e2d8c795b5ad18f8cd9d88c5c1b8
Meiko <meikoprivat@icloud.com>
2022-04-18 13:03:50 +0200

    added get_token()

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
 #include <waifubot.hpp>
-#include <chrono>
 
-int main()
+std::string get_token(std::string filename)
 {
-    WaifuBot bot("token");
+    std::ifstream token_file(filename);
+    std::string token;
+    token_file >> token;
+    return token;
+}
+
+int main(int argc, char* argv[])
+{
+
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <token_file>" << std::endl;
+        return 1;
+    }
+
+    WaifuBot bot(get_token(argv[1]));
 
     bot.cluster_spawn(1, 1);
-    bot.cluster_start();
-
-    while(true) {
-        //wait 5 hours
-        std::this_thread::sleep_for(std::chrono::hours(5));
-    }
+    bot.cluster_start(false);
 
     return 0;
 }
